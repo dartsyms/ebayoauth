2dc93db395013e554acc68a5465cb4d657a8ee8a
Alex Kh <dartsyms@gmail.com>
2018-12-24 17:38:05 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+ruby '2.5.0'
+
+gem 'sinatra', '2.0.5'
+gem 'sinatra-reloader', '1.0'
+gem 'haml', '5.0.4'
---
+++ app.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+load 'ruby/redirect.rb'
+load 'ruby/index.rb'
