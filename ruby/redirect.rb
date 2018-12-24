9043e0edf3579e988e2d9de033e94a3a20f8b854
Alex Kh <dartsyms@gmail.com>
2018-12-24 18:31:39 +0400

    fix path substitution

--- ruby/redirect.rb
+++ ruby/redirect.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 get "/callback/*" do
-  path = request.path.gsub("/callback", "")
+  path = request.path.gsub("/callback/nugenie/", "")
   path_query = "#{path}?#{request.query_string}"
   url = "nugenie://oauth-callback#{path_query}"
   puts url
